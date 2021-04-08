c9c2605fb2185d495f6873eb0bdc3fd92f2a28b0
VolodymyrVi <sqlphpjs@gmail.com>
2021-04-08 17:45:33 +0300

    impruve twig templates

--- index.php
+++ index.php
@@ -2,18 +2,27 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
 
 require __DIR__ . '/vendor/autoload.php';
 
+$loader = new FilesystemLoader('templates');
+$view = new Environment($loader);
+
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+$app->get('/', function (Request $request, Response $response, $args) use ($view) {
+    $body = $view->render('index.twig');
+    $response->getBody()->write($body);
     return $response;
 });
 
-$app->get('/about', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("About Page!");
+$app->get('/about', function (Request $request, Response $response, $args) use ($view){
+    $body = $view->render('about.twig', [
+        'name' => 'VolodymyrVi'
+    ]);
+    $response->getBody()->write($body);
     return $response;
 });
 
