537aa9325d3fc5f7dc00acefb2fbf720e5fc4b8f
VolodymyrVi <sqlphpjs@gmail.com>
2021-04-09 19:32:22 +0300

    added class LatestPots and refactoring

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+
+use Blog\LatestPosts;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
@@ -25,13 +27,14 @@ try {
     die();
 }
 
-$postMapper = new PostMapper($connection);
 
 // Create map
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) use ($view, $postMapper) {
-    $posts = $postMapper->getList('DESC');
+$app->get('/', function (Request $request, Response $response) use ($view, $connection) {
+    $latestPosts = new LatestPosts($connection);
+    $posts = $latestPosts->get(5);
+
     $body = $view->render('index.twig',[
         'posts' => $posts
     ]);
@@ -39,7 +42,7 @@ $app->get('/', function (Request $request, Response $response, $args) use ($view
     return $response;
 });
 
-$app->get('/about', function (Request $request, Response $response, $args) use ($view){
+$app->get('/about', function (Request $request, Response $response) use ($view){
     $body = $view->render('about.twig', [
         'name' => 'VolodymyrVi'
     ]);
@@ -47,7 +50,8 @@ $app->get('/about', function (Request $request, Response $response, $args) use (
     return $response;
 });
 
-$app->get('/{url_key}', function (Request $request, Response $response, $args) use ($view, $postMapper) {
+$app->get('/{url_key}', function (Request $request, Response $response, $args) use ($view, $connection) {
+    $postMapper = new PostMapper($connection);
     $post = $postMapper->getByUrlKey((string) $args['url_key']);
     
     if(empty($post)){
